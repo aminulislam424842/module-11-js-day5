e3c935299137f8f2af6de0790446569cecc0fa47
Aminul islam <aminulislam424842@gmail.com>
2026-07-23 22:50:12 +0600

    feat: remove extra spaces using trim method

---
+++ string/string-task-09-trim-string.js
@@ -0,0 +1,9 @@
+let removeExtraSpaces = "          aminul       ";
+
+console.log("Name Before Trimming:", removeExtraSpaces.length);
+
+let trimmedName = removeExtraSpaces.trim();
+
+console.log(trimmedName);
+
+console.log("Name After Trimming:", trimmedName.length);
