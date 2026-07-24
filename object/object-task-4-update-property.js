47d65ceb37a346ad6a4d039b44562026ff68c344
Aminul islam <aminulislam424842@gmail.com>
2026-07-24 17:45:10 +0600

    feat: update object property using dot and bracket notation

---
+++ object/object-task-4-update-property.js
@@ -0,0 +1,11 @@
+const student = {
+  name: "Aminul islam",
+  age: 42,
+  collage: "Rajshahi Polytechnic Institute"
+}
+
+student.age = 25;
+console.log(student);
+
+student["age"] = 21;
+console.log(student);
