080294980c181c2082f606587703d8d8bee2252d
Aminul islam <aminulislam424842@gmail.com>
2026-07-24 18:51:37 +0600

    feat: create and display nested object

---
+++ object/object-task-14-nested-object.js
@@ -0,0 +1,11 @@
+const information = {
+  name : "Aminul",
+  roll:819683,
+  age:21,
+  education:{
+    school:"surjokona",
+    collage : "Rajshahi Polytechnic Institute"
+  }
+}
+
+console.log(information);
