31058cfb53dd35b1c69d95f1778960515a2be2f5
Aminul islam <aminulislam424842@gmail.com>
2026-07-25 11:49:17 +0600

    feat: build shopping cart total calculator

---
+++ challenge/challenge-03-shopping-cart-total-calculator.js
@@ -0,0 +1,44 @@
+const cart = {
+  product1: {
+    name: "Uma Bag",
+    color: "Black",
+    price: 8200,
+    quantity: 5
+  },
+  product2: {
+    name: "Ruma Dress",
+    color: "Black",
+    price: 30600,
+    quantity: 15
+  },
+  product3: {
+    name: "Vivian Dress",
+    color: "Abract Marble",
+    price: 37400,
+    quantity: 8
+  },
+  product4: {
+    name: "Mila Dress",
+    color: "Black/White",
+    price: 45000,
+    quantity: 10
+  },
+  product5: {
+    name: "Woven Evening Clutch",
+    color: " Gold",
+    price: 13800,
+    quantity: 12
+  }
+}
+let grandTotal = 0;
+
+for (let key in cart) {
+  let subPrice = cart[key].price;
+  let subQuantity = cart[key].quantity;
+  let subTotal = subPrice * subQuantity;
+  console.log(cart[key].name, ":", subTotal);
+
+  grandTotal += subTotal;
+}
+
+console.log("Grand Total :", grandTotal);
